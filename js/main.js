4a9d2b19e7e2c5c84b463d8ec66f8e4e8f5a30a0
01Eddie <jesus.11.97@hotmail.com>
2020-10-04 22:25:29 -0500

    [modified] main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  var element = document.getElementById('index');
+  element.classList.toggle("darkmode1");
+  var element = document.getElementById('about');
+  element.classList.toggle("darkmode2");
+  var element = document.getElementById('work_photos');
+  element.classList.toggle("darkmode3");
+  var element = document.getElementById('resume');
+  element.classList.toggle("darkmode4");
+  var element = document.getElementById('localization');
+  element.classList.toggle("darkmode5");
+  var element = document.getElementById('contact');
+  element.classList.toggle("darkmode6");
+  var element = document.getElementById('details');
+  element.classList.toggle("darkmode7");
+  var element = document.getElementById('skills');
+  element.classList.toggle("darkmode8");
+  var element = document.getElementById('languajes');
+  element.classList.toggle("darkmode9");
+  var element = document.getElementById('testimonials');
+  element.classList.toggle("darkmode10");
+}
