af002d70c655d5ddd163233bb09cb43d25e82937
HugoAlvarado01 <alvaradohugo774@gmail.com>
2025-07-30 20:15:48 -0500

    [ADD] next.config.js for using js file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+};
+
+export default nextConfig;
